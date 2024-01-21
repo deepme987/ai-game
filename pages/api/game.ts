bb7a8b7369428ca3bfd6ff25297754d8ef8f1825
Harrison <harrison.vanderbyl@gmail.com>
2024-01-20 17:59:55 -0800

    add api

---
+++ pages/api/game.ts
@@ -0,0 +1,76 @@
+import { User } from '@/types'
+import type { NextApiRequest, NextApiResponse } from 'next'
+ 
+type ResponseData = {
+  message: string
+}
+
+const data: {
+    [key: string]: {
+        players: {
+            [key: string]: User
+        }
+        started: boolean
+        currentprompt: string
+        currentoptions: string[]
+        currentresponses: {
+            [key: string]: string
+        }
+    }   
+}
+= {
+
+}
+ 
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  // get session id from url
+const sessionId = req.query.sessionId as string; 
+if (!sessionId) {
+    res.status(400).json({ message: 'No session id provided' })
+    return;
+}
+// get player id from url
+const playerId = req.query.playerId as string;
+if (!playerId) {
+    res.status(400).json({ message: 'No player id provided' })
+    return;
+}
+
+    // if post request add player to session
+    if (req.method === 'POST') {
+        // get player name from body
+        const playerName = req.body.playerName;
+        // add player to session
+        data[sessionId].players[playerId] = {
+            id: playerId,
+            name: playerName
+        }
+
+        const response = req.body.response;
+
+        data[sessionId].currentresponses[playerId] = response;
+
+    }
+
+    // if put request update session prompt and options
+    if (req.method === 'PUT') {
+        // get prompt from body
+        const prompt = req.body.prompt;
+        // get options from body
+        const options = req.body.options;
+        // update session prompt and options
+        data[sessionId].currentprompt = prompt;
+        data[sessionId].currentoptions = options;
+
+        // clear responses
+        data[sessionId].currentresponses = {};
+    }
+
+
+
+    return data[sessionId];
+
+}
