a9acb67769200e594cdbe39a129e104605ec2191
Deep Mehta <deepme987@gmail.com>
2024-01-20 18:14:52 -0800

    chore: paths

--- src/app/game-session/[sessionId]/[userid]/page.tsx
+++ src/app/game-session/[sessionId]/[userid]/page.tsx
@@ -1,6 +1,6 @@
 'use client';
-import PlayersList from "@/app/game-session/[sessionId]/players-list";
-import GamePage from "@/app/game-session/[sessionId]/game-page";
+import PlayersList from "@/app/game-session/[sessionId]/[userid]/players-list";
+import GamePage from "@/app/game-session/[sessionId]/[userid]/game-page";
 
 export default function GameSessionPage() {  // Function to handle the removal of users. Placeholder for now.
 
