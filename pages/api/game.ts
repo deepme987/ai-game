cba942aca12fca83140a5d7aa37b5d5c494aa4e5
Harrison <harrison.vanderbyl@gmail.com>
2024-01-20 18:54:19 -0800

    api complete

--- pages/api/game.ts
+++ pages/api/game.ts
@@ -1,20 +1,44 @@
 import { GameData, User } from '@/types'
+import { existsSync, readFileSync, writeFileSync } from 'fs'
 import type { NextApiRequest, NextApiResponse } from 'next'
  
-type ResponseData = {
-  message: string
+// get and store data
+type Data = {
+    [key: string]: GameData
 }
 
-const data: {
-    [key: string]: GameData  
+const getItem = (id: string) => {
+    // write to file system
+    if (!existsSync(id+'.json')) {
+        // create file
+        writeFileSync(id+'.json', '{}', 'utf-8');
+    }
+    return readFileSync(id+'.json', 'utf-8');
 }
-= {
 
+const setItem = (id: string, data: string) => {
+    writeFileSync(id+'.json', data, 'utf-8');
+}
+
+const storedata = (id: string, data: GameData) => {
+    if (getItem(id) === null) {
+        setItem(id, JSON.stringify({}));
+    }
+    
+    setItem(id, JSON.stringify(data));
+}
+
+const getdata = (id: string) => {
+    if (getItem(id) === null) {
+        setItem(id, JSON.stringify({}));
+    }
+    const ldata = JSON.parse(getItem(id) || '{}');
+    return ldata;
 }
  
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<ResponseData>
+  res: NextApiResponse<GameData | { message: string }>
 ) {
   // get session id from url
     const sessionId = req.query.sessionId as string; 
@@ -22,6 +46,9 @@ export default function handler(
         res.status(400).json({ message: 'No session id provided' })
         return;
     }
+
+    // get data from storage
+    const data = getdata(sessionId);
     // get player id from url
     const userId = req.query.userId as string;
     if (!userId) {
@@ -31,6 +58,7 @@ export default function handler(
 
     // if post request add player to session
     if (req.method === 'POST') {
+        console.log(req.body);
         // get player name from body
         const playerName = req.body.playerName;
 
@@ -56,6 +84,9 @@ export default function handler(
 
         data[sessionId].gameData.currentResponses[userId] = response;
 
+        // store data
+        storedata(sessionId, data[sessionId]);
+
     }
 
     // if put request update session prompt and options
@@ -70,8 +101,16 @@ export default function handler(
 
         // clear responses
         data[sessionId].gameData.currentResponses = {};
-    }
 
-    return data[sessionId];
+        // store data
+        storedata(sessionId, data[sessionId]);
+    }
+    console.log(data[sessionId]);
+    console.log(data)
+    // store data
+    storedata(sessionId, data[sessionId]);
+    // respond with session data
+    res.send(data[sessionId])
+    return;
 
 }
